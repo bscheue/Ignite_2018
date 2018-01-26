68c75a2a126a5b6710d0018ad1664282bf3c93eb
Brian Scheuermann <brian@Brians-Air.fios-router.home>
2018-01-26 01:06:53 -0500

    moved some files

--- wk1/key.py
+++
@@ -1,9 +0,0 @@
-def roots(a, b, c):
-    disc = b ** 2 - 4 * a * c
-    if(disc < 0): return None
-    elif(disc == 0): return - b / (2 * a)
-    else:
-        r1 = (- b + Math.sqrt(disc)) / (2 * a)
-        r2 = (- b - Math.sqrt(disc)) / (2 * a)
-        return r1, r2
-
--- wk1/ticTacToeGame.py
+++
@@ -1,3 +0,0 @@
-def print_board(a):
-    for row in a:
-        print(row)
