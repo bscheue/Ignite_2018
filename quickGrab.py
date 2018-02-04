60b2fa6b269422c035086713ef3137fb89f52a6e
Otis Smith <otissmith@Otiss-MBP.wv.cc.cmu.edu>
2018-02-04 13:30:03 -0500

    adding stuff to quickGrab

--- quickGrab.py
+++ quickGrab.py
@@ -1,1 +1,16 @@
-print ("hellooo world")
+import ImageGrab
+import os
+import time
+ 
+def screenGrab():
+    box = ()
+    im = ImageGrab.grab()
+    im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) +
+'.png', 'PNG')
+ 
+def main():
+    screenGrab()
+ 
+if __name__ == '__main__':
+    main()
+
